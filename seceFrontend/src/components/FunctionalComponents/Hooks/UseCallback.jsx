ec783dede29cd6f3b41b73b03be60b5d24e9fdbb
BALASURYA45 <balasurya.d2023cse@sece.ac.in>
2025-01-23 15:50:37 +0530

    anday10

--- seceFrontend/src/components/FunctionalComponents/Hooks/UseCallback.jsx
+++ seceFrontend/src/components/FunctionalComponents/Hooks/UseCallback.jsx
@@ -1,10 +1,30 @@
-const UseCallback = () => {
+import { useState } from "react";
+import ListItems from "./List";
+import { useCallback } from "react";
+const UseCallback=()=>{
     var[num,setNum]=useState(0);
-    return (
-      <div>
-        <h2>Welcome to UseCallback Page</h2>
-        <input type="number" value={num} onChange={(e)=>setNum(e.target.value)}></input>
-      </div>
+    var[dark,setDark]=useState(true);
+    var styling={
+      backgroundColor:dark?"black":"white",
+      color:dark?"white":"black"
+    }
+    var getItems=useCallback((inc)=>{return[num+inc+1,num+inc+2,num+inc+3]},[num])
+
+    return(
+        <div style={styling}>
+          <button onClick={()=>setDark(dark =>!dark)}>Theme Change</button>
+            <h2>This Page is meant for UseCallback Hooks!!</h2>
+            <input 
+              type="number" 
+              value={num} 
+              onChange={
+                (e)=>{setNum(parseInt(e.target.value))
+                }}/>
+
+               <h4> The number is {num}</h4>
+               <ListItems func={getItems}/>
+        </div>
     );
-  };
-  export default UseCallback;
+
+};
+export default UseCallback;
